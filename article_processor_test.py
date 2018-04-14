ad0cdf4d074b60c412deac84d6c4cf3f881c974d
Muhammad Farag <MuhammadFarag@Gmail.com>
2018-04-14 17:27:30 -0400

    test processor will not mark articles that has no empty author

--- article_processor_test.py
+++ article_processor_test.py
@@ -4,11 +4,14 @@ import article_processor as processor
 
 
 class TestLineParsing(unittest.TestCase):
-    article = {'author': '', 'content': 'content body', 'rating': 1, 'errors': []}
-    article2 = {'author': 'Muhammad', 'content': 'content body', 'rating': 1, 'errors': []}
 
     def test_processor_marks_articles_that_has_empty_author(self):
-        self.assertIn('No_Author', processor.inspect_empty_author(self.article)['errors'])
+        article = {'author': '', 'content': 'content body', 'rating': 1, 'errors': []}
+        self.assertIn('No_Author', processor.inspect_empty_author(article)['errors'])
+
+    def test_processor_will_not_mark_articles_that_has_no_empty_author(self):
+        article = {'author': 'Muhammad', 'content': 'content body', 'rating': 1, 'errors': []}
+        self.assertFalse(processor.inspect_empty_author(article)['errors'], 'No errors should be found in errors list')
 
 
 if __name__ == '__main__':
