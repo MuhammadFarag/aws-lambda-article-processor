ec6180e539e06af9a0d18cc4e32b04afa744bd95
Muhammad Farag <MuhammadFarag@Gmail.com>
2018-04-14 17:16:47 -0400

    While python mutates the map passed in, we shouldn't inspect it for changes, instead we should inspect the returned map. Since when deploying lambda on aws, aws is only concerned with the return.
    Since eventually, we will be calling lambdas one from the other, this might change as we go forward with our implementation.

--- article_processor.py
+++ article_processor.py
@@ -1,3 +1,6 @@
 def inspect_empty_author(article):
     if len(article['author']) == 0:
         article['errors'].append('No_Author')
+        return article
+    else:
+        return article
--- article_processor_test.py
+++ article_processor_test.py
@@ -4,15 +4,11 @@ import article_processor as processor
 
 
 class TestLineParsing(unittest.TestCase):
+    article = {'author': '', 'content': 'content body', 'rating': 1, 'errors': []}
+    article2 = {'author': 'Muhammad', 'content': 'content body', 'rating': 1, 'errors': []}
 
     def test_processor_marks_articles_that_has_empty_author(self):
-        article = {'author': '', 'content': 'content body', 'rating': 1, 'errors': []}
-        article2 = {'author': 'Muhammad', 'content': 'content body', 'rating': 1, 'errors': []}
-        articles = [article, article2]
-
-        for a in articles:
-            processor.inspect_empty_author(a)
-        self.assertIn('No_Author', articles[0]['errors'])
+        self.assertIn('No_Author', processor.inspect_empty_author(self.article)['errors'])
 
 
 if __name__ == '__main__':
